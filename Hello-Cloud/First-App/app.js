784d4c2880a935bcb7652b8a70a6d303acca63a8
Edward Thang <RandomClown@gmail.com>
2013-09-03 22:10:44 -0700

    Now works for local & Cf servers

--- Hello-Cloud/First-App/app.js
+++ Hello-Cloud/First-App/app.js
@@ -1,28 +1,28 @@
+module.exports=function() {
+	/*
+	 * Module dependencies.
+	 */
+	var express=require('express');
+	var http=require('http');
 
-/**
- * Module dependencies.
- */
+	var app=express();
 
-require("cf-autoconfig");
-var express = require('express');
-var http = require('http');
+	app.configure(function() {
+		app.set('port', process.env.PORT||3000);
+		app.set('views', __dirname+'/views');
+		app.engine('html', require('ejs').renderFile);
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use('/', express.static(__dirname+"/public/images"));
+	});
 
-var app = express();
-
-app.configure(function(){
-	app.set('port',process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.engine('html',require('ejs').renderFile);
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use('/', express.static(__dirname + "/public/images"));
-});
-
-app.get('/',function(req,res){
-	res.render('stable.html');
-});
+	app.get('/', function(req, res) {
+		res.render('stable.html');
+	});
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+	http.createServer(app).listen(app.get('port'), function() {
+		console.log('Express server listening on port '+app.get('port'));
+	});
+
+};
