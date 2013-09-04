2d3e274de2362940f0a9e13e9641ea14ce1a3262
RandomNPC <bluedoctor7@aol.com>
2013-09-04 00:14:57 -0700

    Added AJAX callback on Game Over
    This commit has been tested and deemed stable
    
    -Added callback to Game Over on app.js
    -Added emitting callback to app.js in stable.html the actual game.
    
    Other:
    -Added mongojs module in package.json

--- Hello-Cloud/First-App/app.js
+++ Hello-Cloud/First-App/app.js
@@ -4,8 +4,14 @@ module.exports=function() {
 	 */
 	var express=require('express');
 	var http=require('http');
+	var mongo = require('mongojs').connect('localhost/Mathius',['scores']);
 
 	var app=express();
+	
+	function Score(username,score){
+		this.username = username;
+		this.score = score;
+	}
 
 	app.configure(function() {
 		app.set('port', process.env.PORT||3000);
@@ -23,6 +29,16 @@ module.exports=function() {
 
 	http.createServer(app).listen(app.get('port'), function() {
 		console.log('Express server listening on port '+app.get('port'));
+	});
+	
+	app.get('/gameover/*',function(req,res){
+		console.log('I made it');
+		console.log('Final score ' + req.params[0]);
+		//check to see if in top 10
+		//if in top 10, ask for input name
+		//have mongodb save the score
+		
+		
 	});
 
 };
