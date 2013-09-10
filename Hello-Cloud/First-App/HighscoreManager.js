d10f87ec1e2f9ed49064d83fd6d9c5ec612b627d
RandomNPC <bluedoctor7@aol.com>
2013-09-10 14:26:13 -0700

    Recoding mongo II
    Working code for mongo database writing. Checks to see if there is a
    highscore!

--- Hello-Cloud/First-App/HighscoreManager.js
+++ Hello-Cloud/First-App/HighscoreManager.js
@@ -11,9 +11,25 @@ var HighscoreManager = function(){
 		db.scores.save(obj,function(err, res){
 			if(!res || err) console.log('error saving item to server: ' + err);
 			db.scores.find({ score : { $lt : obj.score}}).count(function(err,res){
-				console.log(res);
+				if(res>0) console.log('we have a high score!');
+				else console.log('we dont have a high score');
+				db.scores.find().sort({score : 1}).limit(1,function(err,res){
+					res.forEach(function(sub){
+						db.scores.remove(sub);
+					});
+				});
 			});
 		});
+	}
+	
+	this.create = function(){
+		db.scores.drop();
+		db.scores.save(new Highscore(503,'Pandy'),function(err,res){});
+		db.scores.save(new Highscore(243,'Mandy'),function(err,res){});
+		db.scores.save(new Highscore(878,'Moore'),function(err,res){});
+		db.scores.save(new Highscore(59,'Judy'),function(err,res){});
+		db.scores.save(new Highscore(754,'Pat'),function(err,res){});
+		db.scores.save(new Highscore(85,'Junkie'),function(err,res){});
 	}
 }
 util.inherits(HighscoreManager,events.EventEmitter);
--- Hello-Cloud/First-App/Mytest.js
+++ Hello-Cloud/First-App/Mytest.js
@@ -3,4 +3,5 @@ var Highscore = require('./Highscore.js');
 
 var test = new HighscoreManager();
 
-test.add(new Highscore(106,'Murphy'));
+test.add(new Highscore(72,'Gatchapon!'));
+//test.create();
