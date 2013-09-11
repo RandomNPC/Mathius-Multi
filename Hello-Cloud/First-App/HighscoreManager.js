f4fa8cf42925585d7fb50651f2cea28b10d2eaf2
RandomNPC <bluedoctor7@aol.com>
2013-09-11 09:37:09 -0700

    db now has modes!

--- Hello-Cloud/First-App/HighscoreManager.js
+++ Hello-Cloud/First-App/HighscoreManager.js
@@ -3,7 +3,7 @@ var events = require('events');
 var db = require('mongojs').connect('localhost/mathiushighscores',['scores']);
 
 var Highscore = require('./Highscore.js');
-
+var MAX = 7;
 
 var HighscoreManager = function(){
 	
@@ -11,19 +11,58 @@ var HighscoreManager = function(){
 		db.scores.save(obj,function(err, res){
 			if(!res || err) console.log('error saving item to server: ' + err);
 			db.scores.find({ score : { $lt : obj.score}}).count(function(err,res){
-				if(res>0) console.log('we have a high score!');
-				else console.log('we dont have a high score');
-				db.scores.find().sort({score : 1}).limit(1,function(err,res){
-					res.forEach(function(sub){
-						db.scores.remove(sub);
+				db.scores.find({}).count(function(err,entries){
+					if(res>0 || entries < MAX){
+						console.log('we have a high score!');
+						db.scores.update({$or: [{mode: 'display'},{mode: 'input'}]},{$set : {mode : 'input'}},function(err){});
+						if(entries < MAX){
+							console.log('entries: ' + entries);
+							return;
+						}
+					}
+					else{
+						console.log('we dont have a high score');
+						db.scores.update({$or: [{mode: 'display'},{mode: 'input'}]},{$set : {mode : 'display'}},function(err){});
+					}
+					db.scores.find().sort({score : 1}).limit(1,function(err,res){
+						res.forEach(function(sub){
+							db.scores.remove(sub);
+						});
 					});
 				});
 			});
 		});
 	}
 	
+	/*this.add = function(obj){
+		db.scores.save(obj,function(err,res){
+			if(!res || err) console.log('error saving item to server: ' + err);
+			db.scores.find().count(function(err,res){
+				if(res < MAX){
+					console.log('we have a high score!');
+					return;
+				}
+				else{
+					db.scores.find().sort({score: 1}).limit(1,function(err,res){
+						res.forEach(function(search){
+							db.scores.remove(search);
+						});
+						db.scores.find(obj,function(err,res){
+							console.log(err);
+							console.log(res);
+						})
+					});
+				}
+			});
+		});
+	
+	}*/
+	
+	this.is
+	
 	this.create = function(){
 		db.scores.drop();
+		db.scores.save({mode: 'display'});
 		db.scores.save(new Highscore(503,'Pandy'),function(err,res){});
 		db.scores.save(new Highscore(243,'Mandy'),function(err,res){});
 		db.scores.save(new Highscore(878,'Moore'),function(err,res){});
--- Hello-Cloud/First-App/Mytest.js
+++ Hello-Cloud/First-App/Mytest.js
@@ -3,5 +3,5 @@ var Highscore = require('./Highscore.js');
 
 var test = new HighscoreManager();
 
-test.add(new Highscore(72,'Gatchapon!'));
+test.add(new Highscore(42,'Ed'));
 //test.create();
